6578886ee99a8a3991ecf1ea6f7a5e30df639ba6
Michael Nguyen <nguyenmichaelkhang@gmail.com>
2023-09-22 15:38:11 -0400

    ewdsfasdf

--- VM/Michael/vm.c
+++ VM/Michael/vm.c
@@ -5,6 +5,9 @@
 #include "instruction.h"
 #include "bof.h"
 
+// Reminders
+// Make sure to delete all print statements that aren't necessary
+
 int main(int argc, char *arg[])
 {
     if (argc < 2) // If there are no passed arguments
@@ -17,14 +20,26 @@ int main(int argc, char *arg[])
 
     if (strcmp(arg[1], "-p") == 0) // Uses -p option for tracing
     {
-        printf("Working On OUTPUT (.myo/.out)\n\n");
-        bf = bof_read_open(arg[2]); // Reading the bof file and storing it in bf
+        printf("***Working On TRACING (.myo/.out)***\n");
+        bf = bof_read_open(arg[2]); // Reading the bof file and storing a file pointer to bf
     }
     else // if no -p then print output
     {
-        printf("Working On TRACING (.myp/.lst)\n\n");
+        printf("***Working On OUTPUT (.myp/.lst)***\n");
         bf = bof_read_open(arg[1]); 
     }
+    BOFHeader bfh = bof_read_header(bf);
+    printf("Text Start Address = %u\n",bfh.text_start_address); // Is always 0?
+    printf("Text Length = %u\n",bfh.text_length); // This will be the pc + 4, look at vm_test#.lst
+    printf("Data Start Address = %u\n",bfh.data_start_address); 
+    printf("Data Length = %u\n",bfh.data_start_address);
+    printf("Stack Bottom Addr = %u\n",bfh.stack_bottom_addr); // Is always 4096?
+
+    printf("\n*******************************************************************\n")
+
+
+
+    bof_close(bf); // Done with bof file so close it
     return 0;
 }
 
