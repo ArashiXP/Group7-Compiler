7ff011a9067bde2a729035bd891933362f2c8e5f
Maria Talhaferro <91782437+Mafe77@users.noreply.github.com>
2023-09-23 01:04:14 +0000

    m

--- VM/Michael/readme.md
+++ VM/Michael/readme.md
@@ -4,13 +4,3 @@
 I moved all the stuff I didn't need into files to reduce clogging, so the other test files
 and the asm code.  From what I saw in the slides, the bof, instructions, machine_types, regname,
 and utilities are the only codes we need for the VM
-
-Also made good progress on the output/.myp part of the project.  Need to fix the formatting,
-and eventually make it produce a .myp file since it just prints to terminal
-
-to compile use, also make sure you have the vm.h file
-### gcc -o vm vm.c bof.c utilities.c instruction.c regname.c machine_types.c
-
-to test the .lst's/.myp's
-
-### ./vm -p vm_test#.bof
--- VM/Michael/vm.c
+++ VM/Michael/vm.c
@@ -4,85 +4,40 @@
 #include <string.h>
 #include "instruction.h"
 #include "bof.h"
-#include "utilities.h"
-#include "vm.h"
 
 // Reminders
 // Make sure to delete all print statements that aren't necessary
-// Fix the formatting
-
-// ***************************For Tracing**********************************
-void trace(BOFFILE bf)
-{
-
-}
-// *************************************************************************
-
-// ***************************For Output************************************
-
-// Take in the header file and print out the correct format
-void printOut(FILE *out, BOFFILE bf)
-{
-    BOFHeader bh = bof_read_header(bf);
-    printText(out, bf, bh);
-    printData(out, bf, bh);
-}
-
-// Print the title and first half of the output
-void printText(FILE *out, BOFFILE bf, BOFHeader bh)
-{
-    fprintf(out, "Addr Instruction");
-    newline(out);
-    int length = (bh.text_length / BYTES_PER_WORD);
-    // Will call a function to print the PC and instruction
-    for (int i = 0; i < length; i++)
-        printInstruct(out, instruction_read(bf), i * BYTES_PER_WORD);
-}
-
-// prints the PC and instructions with two spaces
-void printInstruct(FILE *out, bin_instr_t bi, unsigned int i)
-{
-    fprintf(out, "  %3d %s", i, instruction_assembly_form(bi));
-    newline(out);
-}
-
-// This will print the bottom stuff
-void printData(FILE *out, BOFFILE bf, BOFHeader bh)
-{
-    int length = bh.data_length / BYTES_PER_WORD;
-    unsigned int num = bh.data_start_address;
-    while (length > 0)
-    {
-        fprintf(out, "   %u: %d\t", num, bof_read_word(bf));
-        num += 4; // Always increment by four
-        length--;
-    }
-    // reached the end, print default
-    fprintf(out, "%u: 0 ...", num);
-    newline(out);
-}
-
-// *************************************************************************
 
 int main(int argc, char *arg[])
 {
     if (argc < 2) // If there are no passed arguments
+    {
+        printf("No Passed Arguments\n");
         return 0;
+    }
 
     BOFFILE bf; // Store the bof file somewhere
 
-    if (strcmp(arg[1], "-p") == 0) // Uses -p option for output
+    if (strcmp(arg[1], "-p") == 0) // Uses -p option for tracing
     {
-        printf("***Working On OUTPUT (.myp/.lst)***\n"); // TO BE REMOVED
+        printf("***Working On TRACING (.myo/.out)***\n");
         bf = bof_read_open(arg[2]); // Reading the bof file and storing a file pointer to bf
-        printOut(stdout, bf);
     }
-    else // if no -p then print trace
+    else // if no -p then print output
     {
-        printf("***Working On TRACING (.myo/.out)***\n"); // TO BE REMOVED
+        printf("***Working On OUTPUT (.myp/.lst)***\n");
         bf = bof_read_open(arg[1]); 
-        trace(bf); 
     }
+    BOFHeader bfh = bof_read_header(bf);
+    printf("Text Start Address = %u\n",bfh.text_start_address); // Is always 0?
+    printf("Text Length = %u\n",bfh.text_length); // This will be the pc + 4, look at vm_test#.lst
+    printf("Data Start Address = %u\n",bfh.data_start_address); 
+    printf("Data Length = %u\n",bfh.data_start_address);
+    printf("Stack Bottom Addr = %u\n",bfh.stack_bottom_addr); // Is always 4096?
+
+    printf("\n*******************************************************************\n");
+
+
 
     bof_close(bf); // Done with bof file so close it
     return 0;
@@ -90,6 +45,3 @@ int main(int argc, char *arg[])
 
 // To Compile
 // gcc -o vm vm.c bof.c utilities.c instruction.c regname.c machine_types.c
-
-// Only the ./vm -p vm_test#.bof function works
-// the ./vm vm_test#.bof function doesn't exist yet
