39e3065c3e7d1e582268a10d38ea54cc98835b8c
Michael Nguyen <nguyenmichaelkhang@gmail.com>
2023-10-15 20:50:04 -0400

    afg

--- Lexer/Michael/lexer_main.c
+++ Lexer/Michael/lexer_main.c
@@ -8,7 +8,12 @@
 #include "lexer.h"
 #include "pl0_lexer.h"
 
-int main(int argc, char*arg[])
+int main(int argc, char*argv[])
 {
+	argc--;
+	argv++;
+	char *filename = argv[0];
+	lexer_init(filename);
+	lexer_output();
 	return 0;
 }
