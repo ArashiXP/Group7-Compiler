fa915e250413ce96941b949110ee139c04ddbed7
Maria Talhaferro <91782437+Mafe77@users.noreply.github.com>
2023-10-21 04:18:58 +0000

    lexer

---
+++ Lexer/Maria/lexer_main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+#include "ast.h"
+#include "parser_types.h"
+#include "utilities.h"
+#include "lexer.h"
+#include "pl0_lexer.h"
+
+extern int yydebug;
+
+int main(int argc, char *argv[])
+{
+    argc--;
+    argv++;
+    char *filename = argv[0];
+    lexer_init(filename);
+    lexer_output();
+    return 0;
+}
